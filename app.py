2f82501a72165dc67bbeda8be452375b1a7d6246
MSukhbat <suhbatnamuun@gmail.com>
2026-06-09 00:17:25 +0900

    sukhbat new

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # from flask import Flask, render_template
 # from routes.analytics import analytics_bp
 # from routes.auth import auth_bp
@@ -50,33 +49,3 @@ for table, path in files.items():
     print(f"{table} done")
 
 print("All data imported successfully.")
-=======
-from flask import Flask, render_template
-from routes.analytics import analytics_bp
-from routes.auth import auth_bp
-from routes.products import products_bp
-from routes.recommendations import recommendations_bp
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "hackathon-secret-key"
-
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(products_bp)
-    app.register_blueprint(recommendations_bp)
-    app.register_blueprint(analytics_bp)
-
-    @app.route("/")
-    def home():
-        return render_template("dashboard.html")
-
-    return app
-
-
-app = create_app()
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 9708887a48e63c3eb385b09d3af6be2e1ad337fb
